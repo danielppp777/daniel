7bc7cca7320a76bcdc4236a13c38eefc907848f2
danielppp777 <152486230+danielppp777@users.noreply.github.com>
2023-12-01 14:33:24 +0200

    Add files via upload

---
+++ Program (3).cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ex 1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double r = Double.Parse(Console.ReadLine());
+
+            double area = Math.Round(3.14159 * (r * r), 2);
+            double perimeter = Math.Round(2 * 3.14159 * r, 2);
+
+            Console.WriteLine($"Area: {area}");
+            Console.WriteLine($"Perimeter: {perimeter}");
+        }
+    }
+}
