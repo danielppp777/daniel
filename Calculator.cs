3601fcee8625181c73ccb2b369d967702320f5f5
danielppp777 <152486230+danielppp777@users.noreply.github.com>
2023-12-01 14:43:13 +0200

    Add files via upload

---
+++ Calculator.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Преобразувател на числа");
+            Console.WriteLine("1: Двоично към десетично");
+            Console.WriteLine("2: Десетично към двоично");
+
+            int choice = int.Parse(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    ConvertBinaryToDecimal();
+                    break;
+                case 2:
+                    ConvertDecimalToBinary();
+                    break;
+                default:
+                    Console.WriteLine("Невалиден избор.");
+                    break;
+            }
+        }
+
+        static void ConvertBinaryToDecimal()
+        {
+            Console.WriteLine("Въведете двоично число");
+            string binaryStr = Console.ReadLine();
+            int decimalResult = Convert.ToInt32(binaryStr, 2);
+            Console.WriteLine($"Decimal: {decimalResult}");
+        }
+
+        static void ConvertDecimalToBinary()
+        {
+            Console.WriteLine("Въведете десетично число");
+            int decimalNum = int.Parse(Console.ReadLine());
+            string binaryResult = Convert.ToString(decimalNum, 2);
+            Console.WriteLine($"Binary: {binaryResult}");
+        }
+    }
+}
